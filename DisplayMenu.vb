12b9caf06f442260ff45c5d999836cce25dddf6b
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2020-01-17 22:38:28 +0500

    Solution of M/J 2015 Paper 2 Variant 1
    Question is all about sub routines

---
+++ DisplayMenu.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Choice, NoOfAttempts As Integer
+        Const I = 3
+        Do
+            Call DisplayMenu()
+            NoOfAttempts = 0
+            Do
+                Console.Write("Enter choice (1...4) :")
+                Choice = Console.ReadLine
+                NoOfAttempts = NoOfAttempts + 1
+            Loop Until (Choice >= 1 And Choice <= 4) Or NoOfAttempts = I
+
+            If Choice = 1 Then Call ReadFile()
+            If Choice = 2 Then Console.WriteLine("Add Customer Code")
+            If Choice = 3 Then Console.WriteLine("Search Customer Code")
+        Loop Until Choice = 4
+    End Sub
+    Sub DisplayMenu()
+        Console.WriteLine()
+        Console.WriteLine("1. Read Customer File")
+        Console.WriteLine("2. Add Customer")
+        Console.WriteLine("3. Search Customer")
+        Console.WriteLine("4. End")
+        Console.WriteLine()
+    End Sub
+
+    Sub ReadFile()
+        Console.WriteLine("Read file code")
+        Console.ReadKey()
+    End Sub
+End Module
