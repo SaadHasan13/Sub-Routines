accbb54cc0fd997b51af6803fbf95d72159023f8
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2020-02-02 21:27:47 +0500

    Q(5)(c) Program Code Solution M/J 2018 (P2V2)

---
+++ GetNumber.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    Function GetNumber() As String
+        Dim Valid As Boolean
+        Dim MemberShipNo As Integer
+        Do
+            Valid = True
+            Console.Write("Please enter a valid number :")
+            MemberShipNo = Console.ReadLine()
+            If MemberShipNo > 1111 And MemberShipNo < 9999 Then
+                Valid = False
+            End If
+        Loop Until Valid = True
+        Return MemberShipNo.ToString()
+    End Function
+    Sub Main()
+        Dim MemberShipNumber As Integer
+        MemberShipNumber = GetNumber()
+        Console.ReadKey()
+    End Sub
+
+End Module
