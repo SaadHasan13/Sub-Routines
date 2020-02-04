d3f66ae8ce06c58db113f70938b1fb58541532f8
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2020-02-04 23:18:15 +0500

    Solution of program code of M/J 2019 P2 V2 Q6(a)

---
+++ TopLevelInformation.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Function GetInfo() As String
+        Dim ID As String = " "
+        Dim PreferredName As String = " "
+        Dim Valid As Boolean = False
+        While Valid = False
+            Console.WriteLine("Please enter a Valid User ID : ")
+            ID = Console.ReadLine()
+            If Len(ID) = 5 And Left(ID, 1) >= "A" And Left(ID, 1) <= "Z" And IsNumeric(Right(ID, 4)) Then
+                Valid = True
+            End If
+        End While
+        Console.WriteLine("Enter your preferred name : ")
+        PreferredName = Console.ReadLine()
+        Return ID & " * " & PreferredName
+    End Function
+    Sub Main()
+        Dim SchoolRecord As String
+        SchoolRecord = GetInfo()
+        Console.ReadKey()
+    End Sub
+  
+End Module
